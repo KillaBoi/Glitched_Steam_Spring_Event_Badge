0f934447166437b04659b533eecde59c342b4713
ㅤ <46409262+KillaBoi@users.noreply.github.com>
2020-07-01 19:13:16 +0100

    Update index.py

--- index.py
+++ index.py
@@ -5,11 +5,9 @@ defid = "73000"
 key = "YOUR OAUTH TOKEN"
 
 url = 'https://api.steampowered.com/ILoyaltyRewardsService/RedeemPointsForBadgeLevel/v1/'
-data=[('access_token', key), ( 'defid', defid)]
+data=[('access_token', key), ( 'defid', defid), ('num_levels', 'REPLACE_ME_WITH_NUMBER')]
 
 x = requests.post(url, data = data)
-
-print(x.text)
 
 if "communityitemid" in (x.text):
     print("Success!")
